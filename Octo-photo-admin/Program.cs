a01eaf81382d2d3745aa752e753dc5fc3fc35b1b
Babouchot <bastien.maureille@hotmail.fr>
2013-04-09 15:33:10 +0200

    few admin modifs

--- Octo-photo-admin/Program.cs
+++ Octo-photo-admin/Program.cs
@@ -8,25 +8,50 @@ namespace Octo_photo_admin
 {
     class Program
     {
+
+        private static String version = "1.0";
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Bienvenue sur le client d'administration d'Octo-photo (version "
+                + version + ")\n\nUtilisation : \n" +
+                "rm user    Supprimer un utilisateur de la base de données\n" +
+                "rm album   Supprimer un album photo de la base de données\n" +
+                "rm photo   Supprimer une photo de la base de données\n");
+
             while (true)
             {
                 Console.Write(">>>");
                 String line = Console.ReadLine();
-                if (line == "add")
+                switch (line)
                 {
-                    Console.WriteLine("Vous avez choisi d'ajouter une image, veuillez spécifier le chemin : ");
-                    Console.Write("chemin :");
-                    String path = Console.ReadLine();
-                    ImageInterface im = new ImageInterface();
-                }
-                else if (line == "rm")
-                {
-                    Console.WriteLine("Mais t'es pas ouf !!");
+                    case "rm user":
+                        Console.WriteLine("Vous avez choisi de supprimer un utilisateur,\nveuillez spécifier son identifiant : ");
+                        Console.Write("User ID : ");
+                        String user = Console.ReadLine();
+                        Console.WriteLine();
+                        break;
+
+                    case "rm album":
+                        Console.WriteLine("Vous avez choisi de supprimer un album,\nveuillez spécifier son identifiant : ");
+                        Console.Write("Nom de l'album : ");
+                        String nomAlbum = Console.ReadLine();
+                        Console.WriteLine();
+                        break;
+
+                    case "rm photo":
+                        Console.WriteLine("Vous avez choisi de supprimer une photo,\nveuillez spécifier son identifiant : ");
+                        Console.Write("ID de la photo : ");
+                        String photo = Console.ReadLine();
+                        Console.WriteLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Commande non reconnue ! veuillez réessayer.\n");
+                        break;
                 }
             }
-
         }
     }
 }
