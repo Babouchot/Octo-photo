ec8c82732cbd27a22ebed466cb970fda6ec11660
Babouchot <bastien.maureille@hotmail.fr>
2013-04-09 14:21:09 +0200

    Initial commit

---
+++ Octo-photo-admin/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octo_photo_admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
