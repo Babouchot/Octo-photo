3daa0b73edc900a3ddda5cc53bd1ac5a78a365ed
Babouchot <bastien.maureille@hotmail.fr>
2013-04-22 17:49:32 +0200

    wcf

--- Octo-photo-wcf/ImageTransfert.svc.cs
+++ Octo-photo-wcf/ImageTransfert.svc.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
 using System.IO;
-namespace TransfertImageService
+using Octo_photo_library;
+
+namespace Octo_photo_wcf
 {
-    public class ImageTransfert : IImageTransfer
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ImageTransfert" à la fois dans le code, le fichier svc et le fichier de configuration.
+    public class ImageTransfert : IImageTransfert
     {
         // la classe AccesDonnees n’est pas donnée ici 
-        private AccesDonnees accesDonnees = new AccesDonnees();
+        private ImageInterface bdAccess = new ImageInterface();
 
         public String UploadImage(Stream image)
         {
@@ -14,11 +22,14 @@ namespace TransfertImageService
             MemoryStream imageStreamEnMemoire = new MemoryStream();
             image.CopyTo(imageStreamEnMemoire);
             imageBytes = imageStreamEnMemoire.ToArray();
-            String imageID = bdAccess.addImage(imageBytes);
+            //String imageID = bdAccess.addImage(idAlbum, image);
             imageStreamEnMemoire.Close();
             image.Close();
-            return imageID;
+            //return imageID;
+            return "plop";
         }
+
+
         public Stream DownloadImage(String imageID)
         {
             // Récupérer l'image stockée en BDD et la transférer au client
