c92a31ddcb6b832208e4d41ea2aed80871b0442e
Babouchot <bastien.maureille@hotmail.fr>
2013-04-30 14:11:27 +0200

    client test ajouté

---
+++ clientTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace clientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciation de la référence de service 
+            imageTransfert.ImageTransfertClient imageTransfertService = new imageTransfert.ImageTransfertClient();
+            MemoryStream imageStream = new MemoryStream(lireFichier(@"C:\Users\user\Pictures\absorbeur.PNG"));
+            // Appel de notre web method 
+            imageTransfertService.UploadImage(imageStream);
+            Console.Out.WriteLine("Transfert Terminé");
+            Console.ReadLine(); 
+        }
+
+        /// <summary> 
+        /// Lit et retourne le contenu du fichier sous la forme de tableau de byte 
+        /// </summary> 
+        /// <param name="chemin">chemin du fichier</param> 
+        /// <returns></returns> 
+        private static byte[] lireFichier(string chemin)
+        {
+            byte[] data = null;
+            FileInfo fileInfo = new FileInfo(chemin);
+            int nbBytes = (int)fileInfo.Length;
+            FileStream fileStream = new FileStream(chemin, FileMode.Open,
+            FileAccess.Read);
+            BinaryReader br = new BinaryReader(fileStream);
+            data = br.ReadBytes(nbBytes);
+            return data;
+        } 
+    }
+}
