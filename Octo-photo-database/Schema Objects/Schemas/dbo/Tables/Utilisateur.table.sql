74e0cbc005533a9a9a01f14161ba3dfa17ca1161
Babouchot <bastien.maureille@hotmail.fr>
2013-05-08 11:28:17 +0200

    db password

--- Octo-photo-database/Schema Objects/Schemas/dbo/Tables/Utilisateur.table.sql
+++ Octo-photo-database/Schema Objects/Schemas/dbo/Tables/Utilisateur.table.sql
@@ -2,7 +2,7 @@
     [idUtilisateur] INT        IDENTITY (1, 1) NOT NULL,
     [nom]           NCHAR (50) NOT NULL,
     [prenom]        NCHAR (50) NOT NULL,
-    [passwordd]     NCHAR (30) NOT NULL
+    [password]     NCHAR (30) NOT NULL
 );
 
 
