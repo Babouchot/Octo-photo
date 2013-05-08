27421f8a4d797fd8598fd8494b59d3ad7cc46e13
Babouchot <bastien.maureille@hotmail.fr>
2013-05-08 18:16:39 +0200

    album

--- ClientWeb/VisualiserAlbum.aspx.cs
+++ ClientWeb/VisualiserAlbum.aspx.cs
@@ -11,27 +11,37 @@ namespace ClientWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // On récupére la valeur des paramètres
+            String imageId = Request.QueryString["ImageID"];
+            String albumId = Request.QueryString["AlbumID"];
+            // Si ces paramètres ne sont pas nuls
+            if (imageId != null && albumId != null)
+            {
+                NumeroAlbum.Text = albumId;
+                ImageTransfertServiceReference.ImageTransfertClient imageTransfert =
+                    new ImageTransfertServiceReference.ImageTransfertClient();
+                try
+                {
+                    string[] album = imageTransfert.getAlbum(int.Parse(albumId));
+                    albumImages.DataSource = album;
+                }
+                catch (FormatException)
+                {
+                    System.Diagnostics.Debug.WriteLine("Un nombre entier est requis");
+                }
+            }
         }
 
         protected void Visualiser_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("visualiser");
-            ImageTransfertServiceReference.ImageTransfertClient imageTransfert =
-                new ImageTransfertServiceReference.ImageTransfertClient();
-            try
-            {
-                byte[][] album = imageTransfert.getAlbum(int.Parse(NumeroAlbum.Text));
-            }
-            catch (FormatException)
-            {
-                System.Diagnostics.Debug.WriteLine("Un nombre entier est requis");
-            }
+            ImageCourante.ImageUrl = "VisualiserAlbum.aspx?ImageID=" + albumImages.SelectedValue +
+                "&AlbumID=" + NumeroAlbum.Text;
         }
 
         protected void albumImages_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ImageCourante.ImageUrl = "VisualiserAlbum.aspx?ImageID=" + albumImages.SelectedValue;
+            ImageCourante.ImageUrl = "VisualiserAlbum.aspx?ImageID=" + albumImages.SelectedValue +
+                "&AlbumID=" + NumeroAlbum.Text;
         }
     }
 }
